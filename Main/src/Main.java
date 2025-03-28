673cd10b924b0d7387a1cdf4e5cd7f1bd9b6ebdc
MCChowtheGreat <chow8801@student.acitech.org>
2025-03-28 14:22:14 -0400

    3/28/25
    Added barbarian subclass
    defauly constructor not working.

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -3,9 +3,11 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-Character c1 = new Character("Hero", 5, 20, 10, 10, 10, 10, 10, 10, 10, );
+Character c1 = new Character();
 System.out.println(c1);
 
+Barbarian b1 = new Barbarian("Fart", 1);
+System.out.println(b1);
     }
 }
 
@@ -23,8 +25,9 @@ class Character {
     protected ArrayList<String> feats = new ArrayList<>();
 
     public Character(){
-        this("Player", 1, 10, 10, 10, 10, 10, 10, 10, 10, );
+        this("Player",1);
     }
+
     public Character (String name, int level, int hit_points, int armor, int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma, ArrayList feats){
         this.name = name;
         this.level = level;
@@ -37,7 +40,13 @@ class Character {
         this.wisdom = wisdom;
         this.charisma = charisma;
         this.feats = feats;
+        calculateHitPoints();
+        calculateArmorClass();
     }
+
+    public Character(String name, int level) {
+    }
+
     private int dice() {
         Random rand = new Random();
         int [] rolls = new int[4];
@@ -50,7 +59,31 @@ class Character {
                 lowest = rolls[i];
             }
         }
-        return sum = lowest;
+        return sum - lowest;
+    }
+
+    private int getAbilityModifier (int score) {
+        return (score - 10) / 2;
+    }
+
+    private void calculateHitPoints() {
+        int dmg = getAbilityModifier(constitution);
+        if (level == 1) {
+            hit_points = 10 + dmg;
+        }
+        else {
+            hit_points += (level - 1) * (6 + dmg);
+        }
+    }
+
+    private void calculateArmorClass (){
+        armor = 10 + getAbilityModifier(dexterity);
+    }
+
+    public void levelUp() {
+        level++;
+        calculateArmorClass();
+        calculateHitPoints();
     }
 public void addFeat(String feat){
         feats.add(feat);
@@ -59,5 +92,13 @@ public void addFeat(String feat){
         return "Character: " + name + " | Level: " + level + "\n" + "HP: " + hit_points + " | AC: " + armor + "\n" + "STR: " + strength + " | DEX: " + dexterity + " | CON: " + constitution + " | INT: " + intelligence + " | WIS: " + wisdom + " | CHA: " + charisma + "\n" + "Feats: " + feats;
     }
 
-
+}
+class Barbarian extends Character{
+    public Barbarian (String name, int level) {
+        super(name, level);
+        this.strength += 2;
+        this.constitution += 2;
+        addFeat("Rage");
+        addFeat("Unarmored Defense");
+    }
 }
